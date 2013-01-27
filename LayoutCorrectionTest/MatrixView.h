8fe6c4e5773732e8a32e35705bb1c76ad05edf9c
KatagiriSo <katagiri@rodhos.info>
2013-01-27 20:37:36 +0900

    名称変更、MITライセンスの旨をつけた

--- LayoutCorrectionTest/MatrixView.h
+++ LayoutCorrectionTest/MatrixView.h
@@ -3,7 +3,7 @@
 //  LayoutCorrectionTest
 //
 //  Created by katagiri on 2013/01/26.
-//  Copyright (c) 2013年 RodhosSoft. All rights reserved.
+//  Copyright (c) 2013年 RodhosSoft. Distributed under the MIT license.
 //
 
 #import <UIKit/UIKit.h>
