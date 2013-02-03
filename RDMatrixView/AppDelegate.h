2d96ed2191185c28d4b6e26c37d154a39a3ac5fd
KatagiriSo <katagiri@rodhos.info>
2013-02-03 21:05:31 +0900

    rename

---
+++ RDMatrixView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RDMatrixView
+//
+//  Created by katagiri on 2013/01/26.
+//  Copyright (c) 2013年 RodhosSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RDMatrixView/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  RDMatrixView
+//
+//  Created by katagiri on 2013/01/26.
+//  Copyright (c) 2013年 RodhosSoft.Distributed under the MIT license.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <MatrixViewProtocol>
+@property (strong, nonatomic) IBOutlet RDMatrixView *matrixView;
+
+
+
+@end
